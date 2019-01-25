3b8e7d9da1f366b9c8cd6890d9ed3dfc702877a7
Douglass Turner <douglass.turner@gmail.com>
2019-01-24 19:34:06 -0500

    hacking

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 import * as THREE from './three.module.js';
+import OrbitControls from './orbit-controls-es6.js';
 import {rgb2hex, appleCrayonColor } from './ei_color.js';
+
 let main = (threejs_canvas) => {
 
     let renderer = new THREE.WebGLRenderer({ canvas: threejs_canvas, antialias: true });
 
-    // renderer.setClearColor(rgb2hex(255, 0, 0));
     renderer.setClearColor(appleCrayonColor('honeydew'));
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     let camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 3000);
 
+    let controls = new OrbitControls(camera, renderer.domElement);
+
+    camera.position.set(300, 300, 700);
+    let target = new THREE.Vector3(0, 0, -1000);
+    camera.lookAt( target );
+
+    // controls.update();
+    controls.addEventListener("change", () => renderer.render(scene, camera));
+
     let scene = new THREE.Scene();
+    scene.background = new THREE.Color(appleCrayonColor('snow'));
 
     let ambient = new THREE.AmbientLight(rgb2hex(255, 255, 255), 0.5);
     scene.add(ambient);
@@ -19,23 +30,24 @@ let main = (threejs_canvas) => {
     let pointLight = new THREE.PointLight(rgb2hex(255, 255, 255), 0.5);
     scene.add(pointLight);
 
-    let dimen = 100;
+
+    const dimen = 150;
     let geometry = new THREE.CubeGeometry(dimen, dimen/2, 2*dimen);
-    let material = new THREE.MeshLambertMaterial({ color: appleCrayonColor('teal') });
+    let material = new THREE.MeshNormalMaterial();
     let mesh = new THREE.Mesh(geometry, material);
-    mesh.position.set(0, 0, -1000);
+    mesh.position.set(target.x, target.y, target.z);
 
     scene.add(mesh);
 
-    let render = () => {
-        mesh.rotation.x += 0.01;
-        mesh.rotation.y += 0.01;
-        renderer.render(scene, camera);
-        requestAnimationFrame(render);
-    };
+    // let animate = () => {
+    //     requestAnimationFrame( animate );
+    //     controls.update();
+    //     renderer.render( scene, camera );
+    // };
+    //
+    // animate();
 
-    requestAnimationFrame(render);
-
+    renderer.render( scene, camera );
 
 };
 
