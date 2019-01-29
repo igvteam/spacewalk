d79001dc3d6a6b6981bda121ac16142824d1b207
Douglass Turner <douglass.turner@gmail.com>
2019-01-29 16:37:46 -0500

    ingest one molecule data

--- js/main.js
+++ js/main.js
@@ -5,37 +5,63 @@ import {rgb2hex, appleCrayonColor } from './ei_color.js';
 let main = (threejs_canvas) => {
 
     let renderer = new THREE.WebGLRenderer({ canvas: threejs_canvas, antialias: true });
-
     renderer.setClearColor(appleCrayonColor('honeydew'));
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
 
-    // const target = new THREE.Vector3(0, 0, -1000);
-    const target = new THREE.Vector3(0,0,0);
+    const [ near, far, fov ] = [ 1e-1, 1e4, 35 ];
+    let camera = new THREE.PerspectiveCamera(fov, window.innerWidth / window.innerHeight, near, far);
+    let orbitControl = new OrbitControls(camera, renderer.domElement);
+    let scene = new THREE.Scene();
 
-    const near = 1e-1;
-    const far = 1e4;
-    let camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, near, far);
+    setup(scene, renderer, camera, orbitControl, new THREE.Vector3(0,0,0));
+};
+
+let setup = async (scene, renderer, camera, orbitControl, target) => {
+
+    const response = await fetch('data/chr21/onemolecule/000001.txt');
+    const text = await response.text();
+
+    const lines = text.split(/\r?\n/);
+
+    let bbox = lines.shift();
+    const pieces = bbox.split(' ');
+    const dev_null = pieces.shift();
+
+    const [minX, minY, minZ, maxX, maxY, maxZ] = pieces.map((string) => { return parseInt(string, 10)});
+    console.log(minX, minY, minZ, maxX, maxY, maxZ);
+
+    let xyz_list = [];
+    for(let line of lines) {
+        let xyz = line.split(' ').map((string) => {
+            if ('NaN' === string) {
+                return NaN;
+            } else {
+                return parseFloat(string);
+            }
+        });
+
+        console.log(xyz[0], xyz[1], xyz[2]);
+        xyz_list.push(xyz);
+    }
+
     camera.position.set(300, 300, 700);
     camera.lookAt( target );
 
-    let orbitControl = new OrbitControls(camera, renderer.domElement);
     orbitControl.screenSpacePanning = false;
     orbitControl.target = target;
     orbitControl.update();
     orbitControl.addEventListener("change", () => renderer.render(scene, camera));
 
-    let scene = new THREE.Scene();
+
     // scene.background = new THREE.Color(appleCrayonColor('snow'));
-    scene.add( new THREE.GridHelper( 1000, 10 ) );
+    scene.add( new THREE.GridHelper( 2 * Math.max(maxX, maxY, maxZ), 16 ) );
 
     let ambient = new THREE.AmbientLight(rgb2hex(255, 255, 255), 0.5);
     scene.add(ambient);
 
     let pointLight = new THREE.PointLight(rgb2hex(255, 255, 255), 0.5);
     scene.add(pointLight);
-
-
 
     let group = new THREE.Group();
 
@@ -64,6 +90,7 @@ let main = (threejs_canvas) => {
     window.addEventListener( 'resize', onWindowResize, false );
 
     renderer.render( scene, camera );
+
 };
 
 export { main };
