8006ac85a53fe15207374a2ccf166ac6645a4fac
Douglass Turner <douglass.turner@gmail.com>
2019-02-02 09:02:47 -0500

    mockups

---
+++ notes/cube-vert.glsl
@@ -0,0 +1,8 @@
+varying vec3 vWorldDirection;
+#include <common>
+void main() {
+	vWorldDirection = transformDirection( position, modelMatrix );
+	#include <begin_vertex>
+	#include <project_vertex>
+	gl_Position.z = gl_Position.w;
+}
