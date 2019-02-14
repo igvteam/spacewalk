52bf92d24d8fbc5fff3198771cc661d9ab97f2ec
Douglass Turner <douglass.turner@gmail.com>
2019-02-14 15:31:24 -0500

    color ramps

--- js/toolPalette.js
+++ js/toolPalette.js
@@ -7,13 +7,36 @@ class ToolPalette {
         element.setAttribute('id', 'trace3d_tool_palette');
         container.appendChild( element );
 
-        [ 0, 1, 2 ].forEach((ignore) => {
+        [ this.featureRampContext, this.segmentBallRampContext, this.rampContext ] = [ 0, 1, 2 ].map((ignore) => {
 
             const childElement = document.createElement('div');
-            // childElement.setAttribute('id', 'trace3d_tool_palette');
             element.appendChild( childElement );
 
+            const canvas = document.createElement('canvas');
+            childElement.appendChild( canvas );
+
+            fitToContainer(canvas);
+
+            return canvas.getContext('2d');
         });
+
+
+
+
+
+        this.featureRampContext.fillStyle = 'green';
+        this.featureRampContext.fillRect(0, 0, this.featureRampContext.canvas.offsetWidth, this.featureRampContext.canvas.offsetHeight);
+
+        this.segmentBallRampContext.fillStyle = 'purple';
+        this.segmentBallRampContext.fillRect(0, 0, this.segmentBallRampContext.canvas.offsetWidth, this.segmentBallRampContext.canvas.offsetHeight);
+
+        this.rampContext.fillStyle = 'cyan';
+        this.rampContext.fillRect(0, 0, this.rampContext.canvas.offsetWidth, this.rampContext.canvas.offsetHeight);
+
+
+
+
+
 
 
         this.layout(container, element);
@@ -44,5 +67,16 @@ class ToolPalette {
     };
 
 }
+
+let fitToContainer = (canvas) => {
+
+    // Make it visually fill the positioned parent
+    canvas.style.width ='100%';
+    canvas.style.height ='100%';
+
+    // ...then set the internal size to match
+    canvas.width  = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+};
 
 export default ToolPalette;
