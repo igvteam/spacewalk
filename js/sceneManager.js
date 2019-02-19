921952dd80b9e7facb9ea53f7feb65b123bd79cd
Douglass Turner <douglass.turner@gmail.com>
2019-02-19 18:14:12 -0500

    SegmentSelectWidget

--- js/sceneManager.js
+++ js/sceneManager.js
@@ -2,6 +2,7 @@ import * as THREE from "./threejs_es6/three.module.js";
 import { globalEventBus } from "./main.js";
 import CubicMapManager from "./cubicMapManager.js";
 import ToolPalette from "./toolPalette.js";
+import SegmentSelectWidget from "./segmentSelectWidget.js";
 import OrbitalCamera from "./orbitalCamera.js";
 import { getMouseXY } from "./utils.js";
 
@@ -35,6 +36,8 @@ class SceneManager {
         container.appendChild( this.renderer.domElement );
 
         this.toolPalette = new ToolPalette({ container, colors: toolPaletteColors, highlightColor: picker.pickHighlighter.highlightColor });
+
+        this.segmentSelectWidget = new SegmentSelectWidget({ container });
 
         this.picker = picker;
 
---
+++ js/segmentSelectWidget.js
@@ -0,0 +1,55 @@
+import { globalEventBus } from "./main.js";
+import { makeDraggable } from "./draggable.js";
+
+class SegmentSelectWidget {
+    constructor({ container }) {
+
+        const selectContainer = document.createElement('div');
+        selectContainer.className = 'trace3d_tool_palette';
+
+        container.appendChild( selectContainer );
+
+        layout(container, selectContainer);
+
+        this.container = container;
+        this.selectContainer = selectContainer;
+
+        makeDraggable(selectContainer, selectContainer);
+
+        $(window).on('resize.trace3d.segment_select_widget', () => { this.onWindowResize() });
+
+        $(this.selectContainer).on('mouseenter.trace3d.segment_select_widget', (event) => {
+            event.stopPropagation();
+
+            globalEventBus.post({ type: "DidEnterSegmentSelectWidget", data: this });
+        });
+
+        $(this.selectContainer).on('mouseleave.trace3d.segment_select_widget', (event) => {
+            event.stopPropagation();
+
+            globalEventBus.post({ type: "DidLeaveSegmentSelectWidget", data: this });
+        });
+
+    }
+
+    configure({ }) {
+    }
+
+    onWindowResize() {
+        layout(this.container, this.selectContainer);
+    };
+
+}
+
+let layout = (container, element) => {
+
+    // const { left, top, right, bottom, x, y, width, height } = container.getBoundingClientRect();
+    const { width, height } = container.getBoundingClientRect();
+    const domRect = element.getBoundingClientRect();
+
+    const multiple = 5/4;
+    $(element).offset( { left: (multiple * domRect.width), top: ((height - domRect.height)/2) } );
+
+};
+
+export default SegmentSelectWidget;
