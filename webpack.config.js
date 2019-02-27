bdc061c66827abce9d7841c5fc4de0329a3208ad
Douglass Turner <douglass.turner@gmail.com>
2019-02-27 10:25:41 -0500

    webpack.config.js hacking

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
+process.env.INDEX_FILE_SRC = '<script type="module" src="js/app.js"></script>';
+process.env.INDEX_FILE_DST = '<script src="./bundle.js"></script>';
+
 module.exports =
     {
         mode: 'none',
@@ -38,10 +41,18 @@ module.exports =
             [
                 new CopyPlugin([
                     { from:'css/app.css', to:'css' },
-                    { from:'img/*'        },
+                    { from:'img/*'         },
                     { from:'texture/**/*'  },
                     { from:'resource/**/*' },
-                    { from:'favicon.ico'  }
+                    { from:'favicon.ico'   },
+                    {
+                        from: 'index.html',
+                        transform: (content) => {
+                            return content
+                                .toString()
+                                .replace(process.env.INDEX_FILE_SRC, process.env.INDEX_FILE_DST);
+                        }
+                    }
                 ])
 
             ]
