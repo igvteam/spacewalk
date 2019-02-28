4a0c10a382e008b70cfbeeafc48d09d4bfd28cb3
Douglass Turner <douglass.turner@gmail.com>
2019-02-28 09:27:24 -0500

    mesh basic material

--- play/shaders/helpful/mesh_basic_material_frag.glsl
+++ play/shaders/helpful/mesh_basic_material_frag.glsl
@@ -1,15 +1,9 @@
-precision highp float;
-precision highp int;
-#define SHADER_NAME MeshBasicMaterial
 #define GAMMA_FACTOR 2
-uniform mat4 viewMatrix;
-uniform vec3 cameraPosition;
-#define TONE_MAPPING
+
 #ifndef saturate
 	#define saturate(a) clamp( a, 0.0, 1.0 )
 #endif
-uniform float toneMappingExposure;
-uniform float toneMappingWhitePoint;
+
 vec3 LinearToneMapping( vec3 color ) {
 	return toneMappingExposure * color;
 }
@@ -17,20 +11,13 @@ vec3 ReinhardToneMapping( vec3 color ) {
 	color *= toneMappingExposure;
 	return saturate( color / ( vec3( 1.0 ) + color ) );
 }
-#define Uncharted2Helper( x ) max( ( ( x * ( 0.15 * x + 0.10 * 0.50 ) + 0.20 * 0.02 ) / ( x * ( 0.15 * x + 0.50 ) + 0.20 * 0.30 ) ) - 0.02 / 0.30, vec3( 0.0 ) )
-vec3 Uncharted2ToneMapping( vec3 color ) {
-	color *= toneMappingExposure;
-	return saturate( Uncharted2Helper( color ) / Uncharted2Helper( vec3( toneMappingWhitePoint ) ) );
-}
+
 vec3 OptimizedCineonToneMapping( vec3 color ) {
 	color *= toneMappingExposure;
 	color = max( vec3( 0.0 ), color - 0.004 );
 	return pow( ( color * ( 6.2 * color + 0.5 ) ) / ( color * ( 6.2 * color + 1.7 ) + 0.06 ), vec3( 2.2 ) );
 }
-vec3 ACESFilmicToneMapping( vec3 color ) {
-	color *= toneMappingExposure;
-	return saturate( ( color * ( 2.51 * color + 0.03 ) ) / ( color * ( 2.43 * color + 0.59 ) + 0.14 ) );
-}
+
 vec3 toneMapping( vec3 color ) { return LinearToneMapping( color ); }
 
 vec4 LinearToLinear( in vec4 value ) {
@@ -101,11 +88,6 @@ vec4 envMapTexelToLinear( vec4 value ) { return LinearToLinear( value ); }
 vec4 emissiveMapTexelToLinear( vec4 value ) { return LinearToLinear( value ); }
 vec4 linearToOutputTexel( vec4 value ) { return LinearToLinear( value ); }
 
-uniform vec3 diffuse;
-uniform float opacity;
-#ifndef FLAT_SHADED
-	varying vec3 vNormal;
-#endif
 #define PI 3.14159265359
 #define PI2 6.28318530718
 #define PI_HALF 1.5707963267949
@@ -167,152 +149,63 @@ float linearToRelativeLuminance( const in vec3 color ) {
 	vec3 weights = vec3( 0.2126, 0.7152, 0.0722 );
 	return dot( weights, color.rgb );
 }
-#ifdef USE_COLOR
-	varying vec3 vColor;
+
+//
+uniform mat4 viewMatrix;
+
+//
+uniform vec3 cameraPosition;
+
+//
+uniform float toneMappingExposure;
+
+//
+uniform float toneMappingWhitePoint;
+
+//
+uniform vec3 diffuse;
+uniform float opacity;
+
+#ifndef FLAT_SHADED
+varying vec3 vNormal;
 #endif
-#if defined( USE_MAP ) || defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( USE_SPECULARMAP ) || defined( USE_ALPHAMAP ) || defined( USE_EMISSIVEMAP ) || defined( USE_ROUGHNESSMAP ) || defined( USE_METALNESSMAP )
-	varying vec2 vUv;
-#endif
-#if defined( USE_LIGHTMAP ) || defined( USE_AOMAP )
-	varying vec2 vUv2;
-#endif
-#ifdef USE_MAP
-	uniform sampler2D map;
-#endif
-#ifdef USE_ALPHAMAP
-	uniform sampler2D alphaMap;
-#endif
-#ifdef USE_AOMAP
-	uniform sampler2D aoMap;
-	uniform float aoMapIntensity;
-#endif
-#ifdef USE_LIGHTMAP
-	uniform sampler2D lightMap;
-	uniform float lightMapIntensity;
-#endif
-#if defined( USE_ENVMAP ) || defined( PHYSICAL )
-	uniform float reflectivity;
-	uniform float envMapIntensity;
-#endif
-#ifdef USE_ENVMAP
-	#if ! defined( PHYSICAL ) && ( defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( PHONG ) )
-		varying vec3 vWorldPosition;
-	#endif
-	#ifdef ENVMAP_TYPE_CUBE
-		uniform samplerCube envMap;
-	#else
-		uniform sampler2D envMap;
-	#endif
-	uniform float flipEnvMap;
-	uniform int maxMipLevel;
-	#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( PHONG ) || defined( PHYSICAL )
-		uniform float refractionRatio;
-	#else
-		varying vec3 vReflect;
-	#endif
-#endif
-#ifdef USE_FOG
-	uniform vec3 fogColor;
-	varying float fogDepth;
-	#ifdef FOG_EXP2
-		uniform float fogDensity;
-	#else
-		uniform float fogNear;
-		uniform float fogFar;
-	#endif
-#endif
-#ifdef USE_SPECULARMAP
-	uniform sampler2D specularMap;
-#endif
-#if defined( USE_LOGDEPTHBUF ) && defined( USE_LOGDEPTHBUF_EXT )
-	uniform float logDepthBufFC;
-	varying float vFragDepth;
-#endif
-#if 0 > 0
-	#if ! defined( PHYSICAL ) && ! defined( PHONG ) && ! defined( MATCAP )
-		varying vec3 vViewPosition;
-	#endif
-	uniform vec4 clippingPlanes[ 0 ];
-#endif
+
+//
+varying vec3 vWorldPosition;
+varying vec3 vReflect;
+
+//
+varying vec3 vColor;
+varying vec2 vUv;
+
 void main() {
-#if 0 > 0
-	vec4 plane;
-	
-	#if 0 < 0
-		bool clipped = true;
-		
-		if ( clipped ) discard;
-	#endif
-#endif
+
 	vec4 diffuseColor = vec4( diffuse, opacity );
-#if defined( USE_LOGDEPTHBUF ) && defined( USE_LOGDEPTHBUF_EXT )
-	gl_FragDepthEXT = log2( vFragDepth ) * logDepthBufFC * 0.5;
-#endif
-#ifdef USE_MAP
-	vec4 texelColor = texture2D( map, vUv );
-	texelColor = mapTexelToLinear( texelColor );
-	diffuseColor *= texelColor;
-#endif
+
+// texture mapping
+//	vec4 texelColor = texture2D( map, vUv );
+//	texelColor = mapTexelToLinear( texelColor );
+//	diffuseColor *= texelColor;
+
+
 #ifdef USE_COLOR
 	diffuseColor.rgb *= vColor;
 #endif
-#ifdef USE_ALPHAMAP
-	diffuseColor.a *= texture2D( alphaMap, vUv ).g;
-#endif
-#ifdef ALPHATEST
-	if ( diffuseColor.a < ALPHATEST ) discard;
-#endif
-float specularStrength;
-#ifdef USE_SPECULARMAP
-	vec4 texelSpecular = texture2D( specularMap, vUv );
-	specularStrength = texelSpecular.r;
-#else
-	specularStrength = 1.0;
-#endif
-	ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
-	#ifdef USE_LIGHTMAP
-		reflectedLight.indirectDiffuse += texture2D( lightMap, vUv2 ).xyz * lightMapIntensity;
-	#else
-		reflectedLight.indirectDiffuse += vec3( 1.0 );
-	#endif
-#ifdef USE_AOMAP
-	float ambientOcclusion = ( texture2D( aoMap, vUv2 ).r - 1.0 ) * aoMapIntensity + 1.0;
-	reflectedLight.indirectDiffuse *= ambientOcclusion;
-	#if defined( USE_ENVMAP ) && defined( PHYSICAL )
-		float dotNV = saturate( dot( geometry.normal, geometry.viewDir ) );
-		reflectedLight.indirectSpecular *= computeSpecularOcclusion( dotNV, ambientOcclusion, material.specularRoughness );
-	#endif
-#endif
+
+    ReflectedLight reflectedLight = ReflectedLight( vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ), vec3( 0.0 ) );
+
+	reflectedLight.indirectDiffuse += vec3( 1.0 );
 	reflectedLight.indirectDiffuse *= diffuseColor.rgb;
+
 	vec3 outgoingLight = reflectedLight.indirectDiffuse;
-#ifdef USE_ENVMAP
-	#if defined( USE_BUMPMAP ) || defined( USE_NORMALMAP ) || defined( PHONG )
-		vec3 cameraToVertex = normalize( vWorldPosition - cameraPosition );
-		vec3 worldNormal = inverseTransformDirection( normal, viewMatrix );
-		#ifdef ENVMAP_MODE_REFLECTION
-			vec3 reflectVec = reflect( cameraToVertex, worldNormal );
-		#else
-			vec3 reflectVec = refract( cameraToVertex, worldNormal, refractionRatio );
-		#endif
-	#else
-		vec3 reflectVec = vReflect;
-	#endif
-	#ifdef ENVMAP_TYPE_CUBE
-		vec4 envColor = textureCube( envMap, vec3( flipEnvMap * reflectVec.x, reflectVec.yz ) );
-	#elif defined( ENVMAP_TYPE_EQUIREC )
-		vec2 sampleUV;
-		reflectVec = normalize( reflectVec );
-		sampleUV.y = asin( clamp( reflectVec.y, - 1.0, 1.0 ) ) * RECIPROCAL_PI + 0.5;
-		sampleUV.x = atan( reflectVec.z, reflectVec.x ) * RECIPROCAL_PI2 + 0.5;
-		vec4 envColor = texture2D( envMap, sampleUV );
-	#elif defined( ENVMAP_TYPE_SPHERE )
-		reflectVec = normalize( reflectVec );
-		vec3 reflectView = normalize( ( viewMatrix * vec4( reflectVec, 0.0 ) ).xyz + vec3( 0.0, 0.0, 1.0 ) );
-		vec4 envColor = texture2D( envMap, reflectView.xy * 0.5 + 0.5 );
-	#else
-		vec4 envColor = vec4( 0.0 );
-	#endif
+
+	vec3 cameraToVertex = normalize( vWorldPosition - cameraPosition );
+	vec3 worldNormal = inverseTransformDirection( normal, viewMatrix );
+	vec3 reflectVec = reflect( cameraToVertex, worldNormal );
+
+	vec4 envColor = textureCube( envMap, vec3( flipEnvMap * reflectVec.x, reflectVec.yz ) );
 	envColor = envMapTexelToLinear( envColor );
+
 	#ifdef ENVMAP_BLENDING_MULTIPLY
 		outgoingLight = mix( outgoingLight, outgoingLight * envColor.xyz, specularStrength * reflectivity );
 	#elif defined( ENVMAP_BLENDING_MIX )
@@ -320,21 +213,15 @@ float specularStrength;
 	#elif defined( ENVMAP_BLENDING_ADD )
 		outgoingLight += envColor.xyz * specularStrength * reflectivity;
 	#endif
-#endif
+
 	gl_FragColor = vec4( outgoingLight, diffuseColor.a );
-#ifdef PREMULTIPLIED_ALPHA
-	gl_FragColor.rgb *= gl_FragColor.a;
-#endif
-#if defined( TONE_MAPPING )
-  gl_FragColor.rgb = toneMapping( gl_FragColor.rgb );
-#endif
-gl_FragColor = linearToOutputTexel( gl_FragColor );
-#ifdef USE_FOG
-	#ifdef FOG_EXP2
-		float fogFactor = whiteCompliment( exp2( - fogDensity * fogDensity * fogDepth * fogDepth * LOG2 ) );
-	#else
-		float fogFactor = smoothstep( fogNear, fogFar, fogDepth );
-	#endif
-	gl_FragColor.rgb = mix( gl_FragColor.rgb, fogColor, fogFactor );
-#endif
+
+    // pre-multiplied alpha
+//    gl_FragColor.rgb *= gl_FragColor.a;
+
+    // tone mapping
+//    gl_FragColor.rgb = toneMapping( gl_FragColor.rgb );
+
+    gl_FragColor = linearToOutputTexel( gl_FragColor );
+
 }
