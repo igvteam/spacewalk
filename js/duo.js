372466a562901fd7281f6fba0ab86a07a23a1725
Douglass Turner <douglass.turner@gmail.com>
2019-03-04 16:31:23 -0500

    duo build out

--- js/duo.js
+++ js/duo.js
@@ -1,4 +1,4 @@
-import {main} from './main.js';
+import {main} from './duo_main.js';
 
 document.addEventListener("DOMContentLoaded", (event) => {
     main(document.getElementById('trace3d_canvas_container'));
