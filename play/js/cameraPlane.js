7d353e5b4a94d68b8fe6b2d2780194fefaa53c82
Douglass Turner <douglass.turner@gmail.com>
2019-03-17 09:23:20 -0400

    rename. use texture

--- play/js/cameraPlane.js
+++ play/js/cameraPlane.js
@@ -49,11 +49,12 @@ let main = async(threejs_canvas) => {
 
     await setup(scene, renderer, camera, orbitControl);
 
-    renderer.render( scene, camera );
+    renderLoop();
 
 };
+
 let target;
-let mesh;
+let planeMesh;
 let setup = async (scene, renderer, camera, orbitControl) => {
 
     const [ targetX, targetY, targetZ ] = [ 0, 0, 0 ];
@@ -72,7 +73,6 @@ let setup = async (scene, renderer, camera, orbitControl) => {
     orbitControl.screenSpacePanning = false;
     orbitControl.target = target;
     orbitControl.update();
-    orbitControl.addEventListener("change", doRender);
 
     const groundPlane = new THREE.GridHelper(4 * dimen, 4 * dimen, appleCrayonColorHexValue('steel'), appleCrayonColorHexValue('steel'));
     groundPlane.position.set(targetX, targetY, targetZ);
@@ -89,14 +89,15 @@ let setup = async (scene, renderer, camera, orbitControl) => {
 
     diffuseCubicMapManager = new CubicMapManager(diffuseCubicMapMaterialConfig);
 
-    let sphereGeometry = new THREE.SphereBufferGeometry( dimen/2, 32, 16 );
+    const texture = new THREE.TextureLoader().load( '../../texture/uv.png' );
+    const textureMaterial = new THREE.MeshBasicMaterial( { map: texture } );
 
-    let planeGeometry = new THREE.PlaneBufferGeometry( 2, 2, 8, 8 );
+    const sphereMesh = new THREE.Mesh(new THREE.SphereBufferGeometry( dimen/2, 32, 16 ), showSTMaterial);
+    scene.add( sphereMesh );
 
-    mesh = new THREE.Mesh(planeGeometry, showSTMaterial);
-    mesh.matrixAutoUpdate = false;
-
-    scene.add( mesh );
+    planeMesh = new THREE.Mesh(new THREE.PlaneBufferGeometry( 2, 2, 8, 8 ), textureMaterial);
+    planeMesh.matrixAutoUpdate = false;
+    scene.add( planeMesh );
 
     window.addEventListener( 'resize', onWindowResize, false );
 
@@ -108,67 +109,61 @@ matrix4Factory.identity();
 let vector3factory = new THREE.Vector3();
 vector3factory.set(0, 0, 0);
 
-let doRender = () => {
+let renderLoop = () => {
+
+    requestAnimationFrame( renderLoop );
 
     const distanceFromCamera = 0.9 * far;
     // const distanceFromCamera = camera.position.length();
 
-    // Scale camera plane to fill viewing frustrum
+    // A - Scale camera plane to fill viewing frustrum
     const dimension = distanceFromCamera * Math.tan( THREE.Math.degToRad( camera.fov/2 ) );
-    const scale = matrix4Factory.clone().makeScale(camera.aspect * dimension, dimension, 1);
-    // const scale = matrix4Factory.clone().makeScale(2, 2, 1);
+    const A = matrix4Factory.clone().makeScale(camera.aspect * dimension, dimension, 1);
 
-    // 0 - Extract rotation
-    const rotationOnly = matrix4Factory.clone().extractRotation(camera.matrixWorldInverse);
+    // B - Extract rotation and invert by transposing
+    const rotation = matrix4Factory.clone().extractRotation(camera.matrixWorldInverse);
+    const B = rotation.clone().transpose();
 
-    // // 1 - Transpose
-    let A = rotationOnly.clone().transpose();
+    // C - Translate rotated camera plane to camera origin
+    const C = matrix4Factory.clone().makeTranslation(camera.position.x, camera.position.y, camera.position.z);
 
-    // // 2 - Translate rotated camera plane to camera origin
-    const B = matrix4Factory.clone().makeTranslation(camera.position.x, camera.position.y, camera.position.z);
-
-    // // 3 - Position camera plane by translating the distance "cameraNear" along camera look-at vector.
+    // D - Position camera plane by translating it along camera look-at vector direction.
     const translation = vector3factory.clone().subVectors(target, camera.position).normalize().multiplyScalar(distanceFromCamera);
-    const C = matrix4Factory.clone().makeTranslation(translation.x, translation.y, translation.z);
+    const D = matrix4Factory.clone().makeTranslation(translation.x, translation.y, translation.z);
 
-    // A * scale
-    const AScale = scale.clone().premultiply(A);
+    // B * A
+    // const BA = A.clone().premultiply(B);
 
-    // B * A * scale
-    const BAScale = AScale.clone().premultiply(B);
+    // C * B * A
+    // const CBA = BA.clone().premultiply(C);
 
-    // C * B * A * scale
-    const CBAScale = BAScale.clone().premultiply(C);
+    // D * C * B * A
+    // const DCBA = CBA.clone().premultiply(D);
 
-    const cameraPlaneTransform = CBAScale.clone();
-
-    // prettyMatrix4Print('scale', scale);
+    const cameraPlaneTransform = A.clone().premultiply(B).premultiply(C).premultiply(D).clone();
 
     // prettyMatrix4Print('A', A);
     // prettyMatrix4Print('B', B);
     // prettyMatrix4Print('C', C);
+    // prettyMatrix4Print('D', D);
 
-    // prettyMatrix4Print('AScale', AScale);
-    // prettyMatrix4Print('BAScale', BAScale);
-    // prettyMatrix4Print('CBAScale', CBAScale);
-    // prettyMatrix4Print('CBAScale', CBAScale);
+    // prettyMatrix4Print('BA', BA);
+    // prettyMatrix4Print('CBA', CBA);
+    // prettyMatrix4Print('DCBA', DCBA);
 
     // prettyMatrix4Print('camera - matrixWorld', camera.matrixWorld);
     // prettyMatrix4Print('matrixWorldInverse', camera.matrixWorldInverse);
 
-    mesh.matrix.copy( cameraPlaneTransform );
+    planeMesh.matrix.copy( cameraPlaneTransform );
 
     renderer.render(scene, camera);
+
 };
 
 let onWindowResize = () => {
-
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize( window.innerWidth, window.innerHeight );
-
-    doRender();
 };
 
 export { main };
