82ce5604212eaf4f5fe85ad7408c887845450d18
Douglass Turner <douglass.turner@gmail.com>
2019-03-20 16:40:30 -0400

    Navbar. Dropdown menu file load. (#58)
    * navbar. dataFileLoad dropdown stub
    
    * navbar dropdown. dataFile load for file, url, select.
    
    * modal build out

--- js/dataFileLoader.js
+++ js/dataFileLoader.js
@@ -1,121 +1,57 @@
 import igv from '../vendor/igv.esm.js'
-import {makeDraggable} from "./draggable.js";
 import { globalEventBus } from "./eventBus.js";
 
-let currentFile = undefined;
 let currentURL = undefined;
 class DataFileLoader {
 
-    constructor({ container, palette, presentationButton }) {
-
-        layout(container, palette);
-
-        makeDraggable(palette, $(palette).find('.trace3d_card_drag_container').get(0));
-
-        $(window).on('resize.trace3d.data_file_load_widget', () => { this.onWindowResize(container, palette) });
-
-        const $palette = $(palette);
-
-        $palette.on('mouseenter.trace3d.data_file_load_widget', (event) => {
-            event.stopPropagation();
-            globalEventBus.post({ type: "DidEnterGUI" });
-        });
-
-        $palette.on('mouseleave.trace3d.data_file_load_widget', (event) => {
-            event.stopPropagation();
-            globalEventBus.post({ type: "DidLeaveGUI" });
-        });
+    constructor({ $urlModal, $selectModal }) {
 
         // Select
-        const $select = $('#trace3d_data_file_load_select');
+        const $select_container = $selectModal.find('#trace3d_data_file_load_select_container');
+        const $select = $selectModal.find('#trace3d_data_file_load_select');
 
-        const $select_button = $('#trace3d_data_file_load_select_button');
-        $select_button.prop('disabled', true);
-
-        $select.on('change.trace3d_data_file_load_select', (e) => {
-            e.stopPropagation();
-            currentURL = e.target.value;
-            $select_button.prop('disabled', false);
-        });
-
-        const $select_container = $('#trace3d_data_file_load_select_container');
-
-        $select_button.on('click.trace3d_data_file_load_select_button', (e) => {
-            e.stopPropagation();
-            loadURL({ url: currentURL, $spinner: $select_container.find('.spinner-border'), $palette });
-            $select_button.prop('disabled', true);
-            currentURL = undefined;
+        $select.on('change.trace3d_data_file_load_select', async (event) => {
+            event.stopPropagation();
+            await loadURL({ url: event.target.value, $spinner: $select_container.find('.spinner-border'), $modal: $selectModal });
 
             const $option = $select.find('option:first');
             $select.val( $option.val() );
+
         });
 
         // URL
+        const $url_container = $('#trace3d_data_file_load_url_container');
         const $url_input = $('#trace3d_data_file_load_url_input');
-        $url_input.val('');
+        const $url_button = $('#trace3d_data_file_load_url_ok_button');
 
-        const $url_button = $('#trace3d_data_file_load_url_button');
+        $url_input.val('');
         $url_button.prop('disabled', true);
 
         $url_input.on('change.trace3d_data_file_load_url_input', (event) => {
             event.stopPropagation();
-            // console.log('url on change - value ' + event.target.value);
             currentURL = event.target.value;
+            $url_button.prop('disabled', false);
         });
 
-        const $url_container = $('#trace3d_data_file_load_url_container');
-
-        $url_button.on('click.trace3d_data_file_load_url_button', (event) => {
+        $url_button.on('click.trace3d_data_file_load_url_button', async (event) => {
             event.stopPropagation();
             $url_input.trigger('change.trace3d_data_file_load_url_input');
-            loadURL({ url: currentURL, $spinner: $url_container.find('.spinner-border'), $palette });
+            await loadURL({ url: currentURL, $spinner: $url_container.find('.spinner-border'), $modal: $urlModal });
             $url_input.val('');
             currentURL = undefined;
+            $url_button.prop('disabled', true);
         });
 
-        // Local file
-        const $file_input = $('#trace3d_data_file_load_local_file_input');
-        $file_input.val('');
-
-        const $file_label = $('#trace3d_data_file_load_local_file_label');
-        $file_label.text('Choose CSV File');
-
-        const $file_button = $('#trace3d_data_file_load_local_file_button');
-        $file_button.prop('disabled', true);
-
-        $file_input.on('change.trace3d_data_file_load_local_file_input', (event) => {
+        $('#trace3d-file-load-local').on('change.trace3d-file-load-local', (event) => {
             event.stopPropagation();
-            // console.log('file on change - value ' + event.target.value);
-            $file_label.text(event.target.files[0].name);
-            currentFile = event.target.files[0];
-            $file_button.prop('disabled', false);
+            loadFile(event.target.files[0]);
         });
-
-        $file_button.on('click.trace3d_data_file_load_local_file_button', (event) => {
-            event.stopPropagation();
-            loadFile(currentFile);
-            $file_label.text('Choose CSV File');
-            currentFile = undefined;
-            $file_button.prop('disabled', true);
-
-            $palette.hide();
-        });
-
-        $(presentationButton).on('click.trace3d_present_data_file_load_palette', (e) => {
-            $palette.toggle();
-        });
-
-        $palette.hide();
 
     }
 
-    onWindowResize(container, palette) {
-        layout(container, palette);
-    };
-
 }
 
-const loadURL = async ({ url, $spinner, $palette }) => {
+const loadURL = async ({ url, $spinner, $modal }) => {
 
     url = url || '';
 
@@ -129,7 +65,7 @@ const loadURL = async ({ url, $spinner, $palette }) => {
             const urlContents = await igv.xhr.load(url);
             $spinner.hide();
 
-            $palette.hide();
+            $modal.modal('hide');
 
             globalEventBus.post({ type: "DidLoadCSVFile", data: { name: file, payload: urlContents } });
 
@@ -171,18 +107,6 @@ const readFileAsText = file => {
 
         fileReader.readAsText(file);
     });
-};
-
-let layout = (container, element) => {
-
-    // const { left, top, right, bottom, x, y, width, height } = container.getBoundingClientRect();
-    const containerRect = container.getBoundingClientRect();
-    const elementRect = element.getBoundingClientRect();
-
-    const left = (containerRect.width - elementRect.width)/2;
-    const top = 1.25 * elementRect.height;
-    $(element).offset( { left, top } );
-
 };
 
 export default DataFileLoader;
