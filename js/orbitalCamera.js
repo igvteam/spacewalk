2d8034b7ed21e6be23eb7e9948e55ac884b1d891
Douglass Turner <douglass.turner@gmail.com>
2019-05-17 15:56:35 -0400

    update

--- js/orbitalCamera.js
+++ js/orbitalCamera.js
@@ -1,4 +1,4 @@
-import * as THREE from "./threejs_es6/three.module.js";
+import * as THREE from "../node_modules/three/build/three.module.js";
 import OrbitControls from "./threejs_es6/orbit-controls-es6.js";
 import { prettyVector3Print } from "./math.js";
 
--- vendor/juicebox/js/utils.js
+++
@@ -1,8 +0,0 @@
-export const getMouseXY = (domElement, { clientX, clientY }) => {
-
-    // a DOMRect object with eight properties: left, top, right, bottom, x, y, width, height
-    const { left, top, width, height } = domElement.getBoundingClientRect();
-
-    return { x: clientX - left,  y: clientY - top, xNormalized: (clientX - left)/width, yNormalized: (clientY - top)/height };
-
-};
