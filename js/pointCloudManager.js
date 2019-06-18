77ea5baad9007262e19fad58368ce8972912ef60
Douglass Turner <douglass.turner@gmail.com>
2019-06-18 10:27:41 -0400

    Point Cloud Manager update

--- js/pointCloudManager.js
+++ js/pointCloudManager.js
@@ -24,118 +24,73 @@ class PointCloudManager {
 
         console.time(`ingest point-cloud with ${ lines.length } points`);
 
-        this.list = [];
-        let currentKey;
-        let currentObj;
-        let xyzList;
-        let rgbList;
+        let segments = {};
 
-        this.boundingBox = new THREE.Box3();
-        this.boundingSphere = new THREE.Sphere();
-        let point = new THREE.Vector3();
-        let color;
-        lines.forEach((line, index) => {
+        for (let line of lines) {
 
             const tokens = line.split(',');
-            const [ startBP, endBP, sizeKB, x, y, z ] = [ tokens[ 0 ], tokens[ 1 ], tokens[ 2 ], tokens[ 3 ], tokens[ 4 ], tokens[ 5 ] ];
+            const [ startBP, endBP, sizeKB ] = [ tokens[ 0 ], tokens[ 1 ], tokens[ 2 ], tokens[ 3 ], tokens[ 4 ], tokens[ 5 ] ];
+            const key = startBP + '%' + endBP + '%' + sizeKB;
 
-            const key = startBP + '%' + endBP;
-
-            if (undefined === currentKey || (currentKey && key !== currentKey)) {
-
-                if (currentKey) {
-                    currentObj.geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( xyzList, 3 ) );
-                    currentObj.geometry.addAttribute(    'color', new THREE.Float32BufferAttribute( rgbList, 3 ) );
-                    currentObj.geometry.computeBoundingBox();
-                    currentObj.geometry.computeBoundingSphere();
-                }
-
-                let obj =
-                    {
-                        geometry: new THREE.BufferGeometry(),
-                        startBP: parseFloat(startBP),
-                        endBP: parseFloat(endBP),
-                        sizeBP: parseFloat(sizeKB) / 1e3
-                    };
-
-                this.list.push(obj);
-
-                currentObj = obj;
-
-                currentKey = key;
-
-                color = appleCrayonRandomBrightColorThreeJS();
-
-                xyzList = [];
-                rgbList = [];
+            if (undefined === segments[ key ]) {
+                segments[ key ] = [];
             }
 
-            xyzList.push(parseFloat(x), parseFloat(y), parseFloat(z));
+            segments[ key ].push(line);
+        }
 
-            const { r, g, b } = color;
-            rgbList.push(r, g, b);
+        this.list = [];
+        this.boundingBox = new THREE.Box3();
+        this.boundingSphere = new THREE.Sphere();
 
-            point.set(parseFloat(x), parseFloat(y), parseFloat(z));
-            this.boundingBox.expandByPoint(point);
+        let xyz = new THREE.Vector3();
 
-        });
+        const keys = Object.keys(segments);
+        for (let key of keys) {
+
+            const segment = segments[ key ];
+            const [ startBP, endBP, sizeKB ] = key.split('%');
+
+            let obj =
+                {
+                    geometry: new THREE.BufferGeometry(),
+                    startBP: parseFloat(startBP),
+                    endBP: parseFloat(endBP),
+                    sizeBP: parseFloat(sizeKB) / 1e3
+                };
+
+            let xyzList = [];
+            let rgbList = [];
+            let color = appleCrayonRandomBrightColorThreeJS();
+
+            for (let line of segment) {
+
+                const tokens = line.split(',');
+                const [ x, y, z ] = [ tokens[ 3 ], tokens[ 4 ], tokens[ 5 ] ];
+
+                xyzList.push(parseFloat(x), parseFloat(y), parseFloat(z));
+
+                const { r, g, b } = color;
+                rgbList.push(r, g, b);
+
+                xyz.set(parseFloat(x), parseFloat(y), parseFloat(z));
+                this.boundingBox.expandByPoint(xyz);
+
+            }
+
+            obj.geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( xyzList, 3 ) );
+            obj.geometry.addAttribute(    'color', new THREE.Float32BufferAttribute( rgbList, 3 ) );
+            obj.geometry.computeBoundingBox();
+            obj.geometry.computeBoundingSphere();
+
+            this.list.push(obj);
+
+        }
 
         this.boundingBox.getBoundingSphere(this.boundingSphere);
 
         this.startBP = this.list[ 0 ].startBP;
         this.endBP = this.list[ (this.list.length - 1) ].endBP;
-
-        console.timeEnd(`ingest point-cloud with ${ lines.length } points`);
-
-    }
-
-    __ingest({ path, string }){
-
-        this.path = path;
-
-        this.geometry = new THREE.BufferGeometry();
-
-        const rawLines = string.split(/\r?\n/);
-
-        // ingest keys
-        const keys = rawLines.shift().split(',');
-
-
-        // locate index of key for the x-coordinate of each row.
-        const index = keys.indexOf('x');
-
-        const lines = rawLines.filter(rawLine => "" !== rawLine);
-
-        console.time(`ingest point-cloud with ${ lines.length } points`);
-
-        let xyzList = [];
-        let rgbList = [];
-        let points = [];
-        lines.forEach(line => {
-
-            const tokens = line.split(',');
-
-            const [ x, y, z ] = [ parseFloat(tokens[ index + 0 ]), parseFloat(tokens[ index + 1 ]), parseFloat(tokens[ index + 2 ]) ];
-
-            xyzList.push(x, y, z);
-
-            points.push(new THREE.Vector3(x, y, z));
-
-            // const { r, g, b } = appleCrayonRandomBrightColorThreeJS();
-            const { r, g, b } = appleCrayonColorThreeJS('aqua');
-
-            rgbList.push(r, g, b);
-
-        });
-
-        this.geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( xyzList, 3 ) );
-        this.geometry.addAttribute(    'color', new THREE.Float32BufferAttribute( rgbList, 3 ) );
-        this.geometry.computeBoundingBox();
-        this.geometry.computeBoundingSphere();
-
-        // this.convexHullGeometry = new ConvexBufferGeometry(points);
-        // this.convexHullGeometry.computeBoundingBox();
-        // this.convexHullGeometry.computeBoundingSphere();
 
         console.timeEnd(`ingest point-cloud with ${ lines.length } points`);
 
