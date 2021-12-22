a80feb4723acee62acf9a335f6718129ef68faf7
Douglass Turner <douglass.turner@gmail.com>
2021-12-22 08:03:40 -0500

    spacewalkFileLoad - Use https for GSDB file retrieval.

--- js/spacewalkFileLoad.js
+++ js/spacewalkFileLoad.js
@@ -25,12 +25,12 @@ function createSpacewalkFileLoaders ({ rootContainer, localFileInput, urlLoadMod
             title: 'GSDB',
             selectionStyle: 'single',
             pageLength: 100,
-            datasource: new GenericDataSource(gsdbDatasourceConfigurator('http://calla.rnet.missouri.edu/genome3d/GSDB/GSDB_JSON_URL_LIST.txt')),
+            datasource: new GenericDataSource(gsdbDatasourceConfigurator('https://calla.rnet.missouri.edu/genome3d/GSDB/GSDB_JSON_URL_LIST.txt')),
             okHandler: (selections) => {
 
                 if (selections.length > 0) {
                     let { name, url } = selections[ 0 ]
-                    url = `http://${ url }`
+                    url = `https://${ url }`
                     fileLoader.load(url)
                 }
             }
