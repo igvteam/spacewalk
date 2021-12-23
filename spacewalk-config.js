57bd868fafbd14cc7ee2b044dd52cb7d44a58e80
Douglass Turner <douglass.turner@gmail.com>
2021-12-23 08:13:39 -0500

    TODO - Google Authentication - Add spacewalk-site.netlify.app as valid origin

--- spacewalk-config.js
+++ spacewalk-config.js
@@ -4,8 +4,8 @@ const spacewalkConfig =
     {
             genomes,
             trackRegistry,
-            clientId: "661332306814-fmasnut050v7qds33tsa2rtvd5tc06sl.apps.googleusercontent.com",
-            apiKey: "AIzaSyCEmqU2lrAgKxJCbnJX87a5F3c9GejCCLA",
+            // clientId: "661332306814-fmasnut050v7qds33tsa2rtvd5tc06sl.apps.googleusercontent.com",
+            // apiKey: "AIzaSyCEmqU2lrAgKxJCbnJX87a5F3c9GejCCLA",
             igvConfig:
                 {
                         genome: 'hg19',
