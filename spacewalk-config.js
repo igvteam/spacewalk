d363e4a09a4dadd0d20ad37751572f65da5e871e
Douglass Turner <douglass.turner@gmail.com>
2023-01-20 10:01:17 -0500

    Disable Google authentication stuff

--- spacewalk-config.js
+++ spacewalk-config.js
@@ -5,8 +5,8 @@ const spacewalkConfig =
         genomeID: 'hg19',
         genomes,
         trackRegistry,
-        clientId: "661332306814-fmasnut050v7qds33tsa2rtvd5tc06sl.apps.googleusercontent.com",
-        apiKey: "AIzaSyCEmqU2lrAgKxJCbnJX87a5F3c9GejCCLA",
+        // clientId: "661332306814-fmasnut050v7qds33tsa2rtvd5tc06sl.apps.googleusercontent.com",
+        // apiKey: "AIzaSyCEmqU2lrAgKxJCbnJX87a5F3c9GejCCLA",
         igvConfig:
             {
                 // genome: 'hg19',
