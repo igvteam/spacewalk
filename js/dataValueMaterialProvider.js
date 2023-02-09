cc5725982fa586b85087648b8eb5eb8eb7e1a3cc
Douglass Turner <douglass.turner@gmail.com>
2023-02-09 13:30:56 -0500

    DataValue MaterialProvider bug fix - color for feature

--- js/dataValueMaterialProvider.js
+++ js/dataValueMaterialProvider.js
@@ -1,4 +1,4 @@
-import {rgb255Lerp, rgb255ToThreeJSColor} from './color.js'
+import {colorString2Tokens, rgb255, rgb255Lerp, rgb255ToThreeJSColor} from './color.js'
 
 class DataValueMaterialProvider {
 
@@ -16,25 +16,48 @@ class DataValueMaterialProvider {
             const start = (feature.start - startBP) / (endBP - startBP)
             const end = (feature.end - startBP) / (endBP - startBP)
 
-            let colorInterpolant
-            if (undefined === min && undefined === max) {
-                colorInterpolant = 1
+            if (feature.color) {
+                const [ r, g, b ] = colorString2Tokens(feature.color)
+                this.interpolantWindows.push({ start, end, color: rgb255ToThreeJSColor(r, g, b) })
             } else {
-                colorInterpolant = (feature.value - min) / (max - min)
+                let colorInterpolant
+                if (undefined === min && undefined === max) {
+                    colorInterpolant = 1
+                } else {
+                    colorInterpolant = (feature.value - min) / (max - min)
+                }
+
+                this.interpolantWindows.push({ start, end, colorInterpolant })
             }
 
-            this.interpolantWindows.push({ start, end, colorInterpolant })
         }
     }
 
     colorForInterpolant(interpolant) {
 
-        for (let { start, end, colorInterpolant } of this.interpolantWindows) {
+        for (const interpolantWindow of this.interpolantWindows) {
+
+            const { start, end } = interpolantWindow
+
             if (interpolant > start && interpolant < end) {
-                const { r, g, b } = rgb255Lerp(this.colorMinimum, this.colorMaximum, colorInterpolant)
-                return rgb255ToThreeJSColor(r, g, b)
+
+                if (interpolantWindow.color) {
+                    return interpolantWindow.color
+                } else {
+                    const { r, g, b } = rgb255Lerp(this.colorMinimum, this.colorMaximum, interpolantWindow.colorInterpolant)
+                    return rgb255ToThreeJSColor(r, g, b)
+                }
+
             }
+
         }
+
+        // for (let { start, end, colorInterpolant } of this.interpolantWindows) {
+        //     if (interpolant > start && interpolant < end) {
+        //         const { r, g, b } = rgb255Lerp(this.colorMinimum, this.colorMaximum, colorInterpolant)
+        //         return rgb255ToThreeJSColor(r, g, b)
+        //     }
+        // }
 
         const { r, g, b } = this.colorMinimum
         return rgb255ToThreeJSColor(r, g, b)
