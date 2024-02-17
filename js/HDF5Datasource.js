b1ddfb54ec602b68a08d360ae8bb46e070d46a58
turner <douglass.turner@gmail.com>
2024-02-17 16:25:40 -0500

    Bug fix. Must cleanse CNDB xyz data

--- js/HDF5Datasource.js
+++ js/HDF5Datasource.js
@@ -92,16 +92,16 @@ class HDF5Datasource extends DataSourceBase {
         const bpDataset = await this.hdf5.get(`${ this.currentReplicaKey }/genomic_position`)
         this.currentGenomicExtentList = await getGenomicExtentList(bpDataset)
 
-        let j = 0
-        const trace = []
-        for (let v = 0; v < numbers.length; v += 3) {
+        const xyzList = createCleanXYZ(numbers)
 
-            const [ x, y, z ] = numbers.slice(v, v + 3)
+        const trace = []
+        let j = 0
+        for (const xyz of xyzList) {
 
             const object =
                 {
                     interpolant: this.currentGenomicExtentList[ j ].interpolant,
-                    xyz: { x, y, z },
+                    xyz,
                     drawUsage: THREE.StaticDrawUsage
                 }
 
@@ -119,6 +119,61 @@ class HDF5Datasource extends DataSourceBase {
         return undefined
     }
 
+}
+
+function createCleanXYZ(numbers) {
+
+    const bbox = createBoundingBox(numbers)
+    const centroid = { x: bbox.centroid[ 0 ], y: bbox.centroid[ 1 ], z: bbox.centroid[ 2 ] }
+
+    const list = []
+    for (let v = 0; v < numbers.length; v += 3) {
+
+        const [ x, y, z ] = numbers.slice(v, v + 3)
+
+        if ( [ x, y, z ].some(isNaN) ) {
+            // console.warn('is missing data')
+            list.push(centroid)
+        } else {
+            list.push({ x, y, z })
+        }
+
+    }
+
+    return list
+}
+
+function createBoundingBox(numbers) {
+
+    const bbox =
+        {
+            min: [ Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY, Number.POSITIVE_INFINITY ],
+            max: [ Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY ],
+            centroid: [ 0, 0, 0 ],
+        }
+
+    for (let n = 0; n < numbers.length; n += 3) {
+
+        const [ x, y, z ] = numbers.slice(n, n + 3)
+
+        if ( ![ x, y, z ].some(isNaN) ) {
+            // min
+            bbox.min[ 0 ] = Math.min(bbox.min[ 0 ], x)
+            bbox.min[ 1 ] = Math.min(bbox.min[ 1 ], y)
+            bbox.min[ 2 ] = Math.min(bbox.min[ 2 ], z)
+
+            // max
+            bbox.max[ 0 ] = Math.max(bbox.max[ 0 ], x)
+            bbox.max[ 1 ] = Math.max(bbox.max[ 1 ], y)
+            bbox.max[ 2 ] = Math.max(bbox.max[ 2 ], z)
+        }
+    }
+
+    bbox.centroid[ 0 ] = (bbox.min[ 0 ] + bbox.max[ 0 ]) / 2.0
+    bbox.centroid[ 1 ] = (bbox.min[ 1 ] + bbox.max[ 1 ]) / 2.0
+    bbox.centroid[ 2 ] = (bbox.min[ 2 ] + bbox.max[ 2 ]) / 2.0
+
+    return bbox
 }
 
 async function getReplicaKeys(hdf5) {
