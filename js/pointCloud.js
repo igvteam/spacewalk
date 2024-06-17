398a3d38f234c2518573b053a83d849313f76e9c
turner <douglass.turner@gmail.com>
2024-06-17 17:05:27 -0400

    Pointcloud. Clamp point size changes

--- js/pointCloud.js
+++ js/pointCloud.js
@@ -195,7 +195,7 @@ class PointCloud {
 
     updatePointSize(increment) {
 
-        this.pointSize += increment < 0 ? -32 : 32
+        this.pointSize = Math.max(4, this.pointSize + (increment < 0 ? -32 : 32))
         document.querySelector('#spacewalk_ui_manager_pointcloud_point_size_label').innerHTML = `Point Size (${ Math.floor(this.pointSize)} nm)`
 
         this.material.size = this.pointSize
