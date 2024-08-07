3a4a564b721a4eede050d616d8e4e926a8dade73
Douglass Turner <douglass.turner@gmail.com>
2024-08-07 12:54:27 -0400

    Bug fix - Juicebox cross hair repair for session load (#291)

--- js/spacewalkSession.js
+++ js/spacewalkSession.js
@@ -48,11 +48,8 @@ async function loadIGVSession(spacewalk, igv) {
 }
 
 async function loadJuiceboxSession(session) {
-
-    await hic.restoreSession(document.querySelector('#spacewalk_juicebox_root_container'), session)
+    juiceboxPanel.browser = await hic.restoreSession(document.querySelector('#spacewalk_juicebox_root_container'), session)
     juiceboxPanel.configureMouseHandlers()
-
-    // await juiceboxPanel.initialize(document.querySelector('#spacewalk_juicebox_root_container'), session)
 }
 
 async function loadSpacewalkSession (session) {
