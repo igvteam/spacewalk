54d745458ee6d1519cb8420f234bba48e89c3cc8
turner <douglass.turner@gmail.com>
2024-08-21 15:20:20 -0400

    misc

--- js/juicebox/liveContactMapService.js
+++ js/juicebox/liveContactMapService.js
@@ -1,4 +1,4 @@
-import hic from '../../node_modules/juicebox.js/js/index.js'
+import hic from 'juicebox.js'
 import {colorMapManager, ensembleManager, igvPanel, juiceboxPanel} from "../app.js"
 import EnsembleManager from "../ensembleManager.js"
 import LiveContactMapDataSet from "./liveContactMapDataSet.js"
