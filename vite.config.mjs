4826e262c9694327c366ff5007f675e4adba7765
turner <douglass.turner@gmail.com>
2024-10-29 16:55:20 -0400

    Update sass

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,16 @@ export default defineConfig({
     build: {
         target: 'es2020'
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: 'modern-compiler', // or "modern", "legacy"
+                importers: [
+                    // ...
+                ],
+            },
+        },
+    },
     optimizeDeps: {
         esbuildOptions : {
             target: "es2020"
